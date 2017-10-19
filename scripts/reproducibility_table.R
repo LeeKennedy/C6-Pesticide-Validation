ac5e3fc7d8a7a27a458ab0696b1cc58be33ac63a
LeeKennedy <mail@leekennedy.id.au>
2017-10-19 21:25:40 +1100

    Reproducibility across the sets

--- scripts/reproducibility_table.R
+++ scripts/reproducibility_table.R
@@ -6,6 +6,8 @@ library(readxl)
 library(tidyverse)
 library(dts.quality)
 
+options(stringsAsFactors = FALSE)
+
 #### Spreadsheet information -----------------------------
 
 if("Windows" %in% Sys.info()['sysname'] == TRUE){ 
@@ -17,52 +19,90 @@ if("Windows" %in% Sys.info()['sysname'] == TRUE){
 
 sheets <- excel_sheets(path = path)
 
+GC_precision2 <- data.frame(
+        Compound = as.character(),
+        Matrix = as.character(),
+        Repeatability = as.numeric(),
+        Reproducibility = as.numeric())
+
 #### Data Input -----------------------------
 
-GC_data <- read_excel(path, sheet = sheets[1], skip = 4)
+for (m in c(1,3,4,5,6,7)){
 
-#GC_precision <- data.frame(
-#        Compound = as.character(),
-#        Matrix = as.character(),
-#        Repeatability = as.numeric(),
-#        Reproducibility = as.numeric())
+GC_data <- read_excel(path, sheet = sheets[m], skip = 4)
+
+n_rows <- nrow(GC_data)
+
 
 ####   Taking one line ---------------------------------------
 
+for (k in 1:n_rows) {
+        
+n <- ncol(GC_data)
+        
+if(n == 29) {
+        batch1 <- GC_data[k,c(3:9)]
+} else {
+        batch1 <- GC_data[k,c(2:8)]
+}
 
 
-batch1 <- GC_data[1,c(3:9)]
+if(n == 29) {
+        batch2 <- GC_data[k,c(12:18)]
+} else {
+        batch2 <- GC_data[k,c(11:17)]
+}
 
-batch2 <- GC_data[1,c(12:18)]
 for (i in 1:7){
         colnames(batch2)[i] = i 
 }
 
-batch3 <- GC_data[1,c(21:27)]
+if(n == 29) {
+        batch3 <- GC_data[k,c(21:27)]
+} else {
+        batch3 <- GC_data[k,c(20:26)]
+}
+
+
 for (i in 1:7){
         colnames(batch3)[i] = i 
 }
 
 batch_set <- rbind(batch1, batch2, batch3)
+batch_set <- as.data.frame(sapply(batch_set, as.numeric))
 batch_set$Batch <- c("A", "B", "C")
 batch_set <- batch_set[,c(8,1:7)]
 batch_set
 batchlong <- tidyr::gather(data = batch_set, key = Run, value = Value, na.rm = FALSE, `1`, `2`, `3`, `4`, `5`, `6`, `7`)
 
-batch_aov <- aov(Value~Batch, data = batchlong)
-summary(batch_aov)
+batch_aov <- aov(Value~Batch, data = batchlong, na.rm = TRUE)
 
 #Repeatability & Reproducibility
 mean.sqr <- summary(batch_aov)[1][[1]][[3]]
 ncount <- as.numeric(length(batch_aov$effects))/as.numeric(length(batch_aov$coefficients))
-sdr <- sqrt(mean.sqr[2])
+Repeatability <- sqrt(mean.sqr[2])
 interim <- sqrt((mean.sqr[1]-mean.sqr[2])/ncount)
-sdR <- sqrt(sdr^2 + interim^2)
+Reproducibility <- sqrt(Repeatability^2 + interim^2)
 
-GC_precision$Compound <- as.character(GC_precision$Compound)
-GC_precision$Matrix <- as.character(GC_precision$Matrix)
 
-GC_precision <- as.data.frame(cbind(sdr, sdR))
-GC_precision$Matrix <- sheets[1]
-GC_precision$Compound <- as.list((GC_data)[1,1])
+
+GC_precision <- as.data.frame(cbind(Repeatability, Reproducibility))
+
+GC_precision$Matrix <- sheets[m]
+comp <- as.list((GC_data)[k,1])
+GC_precision$Compound <- comp[[1]]
+
 GC_precision <- GC_precision[,c(4,3,1,2)]
+GC_precision2 <- rbind(GC_precision2, GC_precision)
+}
+}
+
+
+
+if("Windows" %in% Sys.info()['sysname'] == TRUE){ 
+        
+        write_csv(GC_wide, "H:/GitHub Projects/C6-Pesticide-Validation/outputs/Reproducibility.csv")
+} else { 
+        write_csv(GC_precision2, "/Users/Study Old/Documents/GitHub/C6 Pesticide Validation/outputs/Reproducibility.csv")
+        
+}
