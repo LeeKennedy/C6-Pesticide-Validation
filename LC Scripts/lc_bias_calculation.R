189c3334e00db88beb06ea9d43ffe5459d4ad915
LeeKennedy <mail@leekennedy.id.au>
2017-10-22 17:45:07 +1100

    LC Bias done.

--- LC Scripts/lc_bias_calculation.R
+++ LC Scripts/lc_bias_calculation.R
@@ -10,16 +10,16 @@ library(dts.quality)
 
 if("Windows" %in% Sys.info()['sysname'] == TRUE){ 
         
-        path <- "H:/GitHub Projects/C6-Pesticide-Validation/data/GCMSMS Validation Data.xlsx" 
+        path <- "H:/GitHub Projects/C6-Pesticide-Validation/data/LCMSMS Validation Data.xlsx" 
 } else { 
-        path <- "~/Documents/GitHub/C6 Pesticide Validation/data/GCMSMS Validation Data.xlsx"
+        path <- "~/Documents/GitHub/C6 Pesticide Validation/data/LCMSMS Validation Data.xlsx"
 }
 
 sheets <- excel_sheets(path = path)
 
 #### Data Input -----------------------------
 
-GC_longer2 <- data.frame(
+LC_longer2 <- data.frame(
         Compound = as.character(),
         Matrix = as.character(),
         Batch = as.numeric(),
@@ -34,73 +34,59 @@ GC_longer2 <- data.frame(
         )
 
 
-for (j in 1:7){
+for (j in seq(from = 3, to = 15, by = 2)){
 
-
-GC_data <- read_excel(path, sheet = sheets[j], skip = 4)
+LC_data <- read_excel(path, sheet = sheets[j], skip = 3)
         
 #### Data Cleaning -----------------------------
         
-n <- ncol(GC_data)
+n <- ncol(LC_data)
         
-if(n == 29) {
-        GC_data1 <- GC_data[,c(1,3:9)]
-} else {
-        GC_data1 <- GC_data[,c(1,2:8)]
-}
-
-GC_data1$Batch <- 1
+LC_data1 <- LC_data[,c(1,2:8)]
+LC_data1$Batch <- 1
 
 
-if(n == 29) {
-        GC_data2 <- GC_data[,c(1, 12:18)]
-} else {
-        GC_data2 <- GC_data[,c(1, 11:17)]
-}
+LC_data2 <- LC_data[,c(1, 10:16)]
 
 for (i in 2:8){
-        colnames(GC_data2)[i] = i-1 
+        colnames(LC_data2)[i] = i-1 
 }
 
-GC_data2$Batch <- 2
+LC_data2$Batch <- 2
 
-if(n == 29) {
-        GC_data3 <- GC_data[,c(1, 21:27)]
-} else {
-        GC_data3 <- GC_data[,c(1, 20:26)]
-}
+LC_data3 <- LC_data[,c(1, 18:24)]
 
 for (i in 2:8){
-        colnames(GC_data3)[i] = i-1 
+        colnames(LC_data3)[i] = i-1 
 }
-GC_data3$Batch <- 3
+LC_data3$Batch <- 3
 
-GC_long <- rbind (GC_data1, GC_data2, GC_data3)
+LC_long <- rbind (LC_data1, LC_data2, LC_data3)
 
-GC_long <- GC_long[,c(1,9,2:8)]
+LC_long <- LC_long[,c(1,9,2:8)]
 
-GC_longer <- tidyr::gather(data = GC_long, key = Result, value = Value, na.rm = FALSE, `1`, `2`, `3`, `4`, `5`, `6`, `7`)
+LC_longer <- tidyr::gather(data = LC_long, key = Result, value = Value, na.rm = FALSE, `1`, `2`, `3`, `4`, `5`, `6`, `7`)
 
-GC_longer$Value <- sapply(GC_longer$Value, as.numeric)
+LC_longer$Value <- sapply(LC_longer$Value, as.numeric)
 
-GC_longer$Matrix <- sheets[j]
-GC_longer$Mean <- 0.075
-GC_longer$SD <- 0.001
-GC_longer$n <- 20
-GC_longer$Units <- "mg/kg"
-GC_longer <- GC_longer[, c(1,5,2,3,6:8,4)]
-GC_longer$pct_sd <- 100*GC_longer$SD/GC_longer$Mean
-GC_longer$bias <- GC_longer$Value-GC_longer$Mean
-GC_longer$pct_bias <- 100*GC_longer$bias/GC_longer$Mean
+LC_longer$Matrix <- sheets[j]
+LC_longer$Mean <- 0.075
+LC_longer$SD <- 0.001
+LC_longer$n <- 20
+LC_longer$Units <- "mg/kg"
+LC_longer <- LC_longer[, c(1,5,2,3,6:8,4)]
+LC_longer$pct_sd <- 100*LC_longer$SD/LC_longer$Mean
+LC_longer$bias <- LC_longer$Value-LC_longer$Mean
+LC_longer$pct_bias <- 100*LC_longer$bias/LC_longer$Mean
 
-GC_longer2 <- rbind(GC_longer2,GC_longer)
+LC_longer2 <- rbind(LC_longer2,LC_longer)
 
 }
 
 
 # Data Input -------------------------------------------------------------
 
-bias <- GC_longer2
+bias <- LC_longer2
 
 
 # Bias--------------------------------------------------------------------
@@ -118,12 +104,16 @@ summary_bias <- bias %>%
 
 summary_bias <- summary_bias[,c(1:7)]
 
-nrsb <- nrow(summary_bias)
+
 summary_bias$Significance <- "temp"
+
+summary_bias <- na.omit(summary_bias)
+
+nrsb <- nrow(summary_bias)
 
 for (l in 1:nrsb){
 
-        if ((2*summary_bias$UoB[l] > abs(summary_bias$av_bias[l])) == TRUE ){
+        if ((2*summary_bias$UoB[l] > abs(summary_bias$av_bias[l])) == TRUE){
                 
                 summary_bias$Significance[l] = "Insignificant"
 } else {
@@ -132,5 +122,5 @@ for (l in 1:nrsb){
 }
        
 
-write.csv(summary_bias, "/Users/Study Old/Documents/GitHub/C6 Pesticide Validation/outputs/Bias_Summary.csv")
+write.csv(summary_bias, "/Users/Study Old/Documents/GitHub/C6 Pesticide Validation/LC outputs/LC_Bias_Summary.csv")
 
